51d3aeeb616be6bc44c48a8c156c9548963f38cc
Dante <bartosz.bk04@gmail.com>
2020-11-27 12:35:38 +0100

    Added Result class

---
+++ MovieSearch/MovieSearch/Helpness/Result.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace MovieSearch.Helpness
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+        public IEnumerable<ErrorMessage> Errors { get; set; }
+
+        public static Result<T> Ok<T>(T value)
+        {
+            return new Result<T>
+            {
+                Success = true,
+                Value = value
+            };
+        }
+
+        public static Result<T> Error<T>(string message)
+        {
+            return Error<T>(string.Empty, message);
+        }
+
+        public static Result<T> Error<T>(string propertyName, string message)
+        {
+            return new Result<T>
+            {
+                Success = false,
+                Errors = new List<ErrorMessage>
+                {
+                    new ErrorMessage
+                    {
+                        ProprtyName = propertyName,
+                        Message = message
+                    }
+                }
+            };
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        public T Value { get; set; }
+    }
+
+    public class ErrorMessage
+    {
+        public string ProprtyName { get; set; }
+        public string Message { get; set; }
+    }
+}
