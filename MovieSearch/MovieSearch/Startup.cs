a6aba8fdb67a58ef6a72bb9df32356e3563827ed
Dante <bartosz.bk04@gmail.com>
2020-11-28 10:18:37 +0100

    Deleted useless code

--- MovieSearch/MovieSearch/Startup.cs
+++ MovieSearch/MovieSearch/Startup.cs
@@ -29,7 +29,6 @@ namespace MovieSearch
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
 
             services.AddTransient<IOMDbService, OMDbService>();
         }
